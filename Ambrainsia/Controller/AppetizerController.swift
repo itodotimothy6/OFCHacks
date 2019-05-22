3c925d6f78300329fcb5d3be3033c25c1734e7f3
Timothy Itodo <itodotimothy6@gmail.com>
2019-05-22 13:49:49 -0400

    Add DrawPad

--- Ambrainsia/Controller/AppetizerController.swift
+++ Ambrainsia/Controller/AppetizerController.swift
@@ -10,16 +10,98 @@ import UIKit
 
 class AppetizerController: UIViewController {
 
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var tempImageView: UIImageView!
+    
+    
+    // MARK: Var for Drawing
+    var lastPoint = CGPoint.zero
+    var color = UIColor.black
+    var brushWidth: CGFloat = 10.0
+    var opacity: CGFloat = 1.0
+    var swiped = false
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(timeToMoveOn), userInfo: nil, repeats: false)
+        Timer.scheduledTimer(timeInterval: 60.0, target: self, selector: #selector(timeToMoveOn), userInfo: nil, repeats: false)
         
     }
     
     @objc func timeToMoveOn() {
         self.performSegue(withIdentifier: "goToDesertIntro", sender: self)
     }
+    
+    
+    //MARK: DrawPad Functions
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        swiped = false
+        lastPoint = touch.location(in: view)
+    }
+
+    func drawLine(from fromPoint: CGPoint, to toPoint: CGPoint) {
+        // 1
+        UIGraphicsBeginImageContext(view.frame.size)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        tempImageView.image?.draw(in: view.bounds)
+        
+        // 2
+        context.move(to: fromPoint)
+        context.addLine(to: toPoint)
+        
+        // 3
+        context.setLineCap(.round)
+        context.setBlendMode(.normal)
+        context.setLineWidth(brushWidth)
+        context.setStrokeColor(color.cgColor)
+        
+        // 4
+        context.strokePath()
+        
+        // 5
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempImageView.alpha = opacity
+        UIGraphicsEndImageContext()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        
+        // 6
+        swiped = true
+        let currentPoint = touch.location(in: view)
+        drawLine(from: lastPoint, to: currentPoint)
+        
+        // 7
+        lastPoint = currentPoint
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !swiped {
+            // draw a single point
+            drawLine(from: lastPoint, to: lastPoint)
+        }
+        
+        // Merge tempImageView into mainImageView
+        UIGraphicsBeginImageContext(mainImageView.frame.size)
+        mainImageView.image?.draw(in: view.bounds, blendMode: .normal, alpha: 1.0)
+        tempImageView?.image?.draw(in: view.bounds, blendMode: .normal, alpha: opacity)
+        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        tempImageView.image = nil
+    }
+
+    
     
 
     
